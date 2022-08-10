c088a5d59f782f8f3f29171efbab4bd80a4bc82d
Jesse Junior <jejunior@support.connectionsacademy.org>
2022-08-10 07:58:57 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroClientes.MVC/CadastroClientes.MVC/Models/ModeloCliente.cs
@@ -0,0 +1,10 @@
+﻿namespace CadastroClientes.MVC.Models
+{
+    public class ModeloCliente
+    {
+        public string CPF { get; set; }
+        public int Idade { get; set; }
+        public string Nome { get; set; }
+        
+    }
+}
