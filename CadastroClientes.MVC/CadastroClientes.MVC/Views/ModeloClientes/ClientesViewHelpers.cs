69b70a5b1086c250b45a8f3c3fb3591914dfc05e
Jesse Junior <jejunior@support.connectionsacademy.org>
2022-08-15 18:55:30 -0300

    finalizado mvc

---
+++ CadastroClientes.MVC/CadastroClientes.MVC/Views/ModeloClientes/ClientesViewHelpers.cs
@@ -0,0 +1,19 @@
+﻿using CadastroClientes.MVC.Models;
+
+namespace CadastroClientes.MVC.Views.ModeloClientes
+{
+    //TODO: DELETAR
+    public static class ClientesViewHelpers
+    {
+        public static string ObterClasseSemaforoParaPoderDeDecisao(PoderDeDecisao poderDeDecisao)
+        {
+            return poderDeDecisao switch
+            {
+                PoderDeDecisao.Limitado => "limitado",
+                PoderDeDecisao.Elevado => "elevado",
+                PoderDeDecisao.Total => "total",
+                _ => throw new NotImplementedException()
+            };
+        }
+    }
+}
