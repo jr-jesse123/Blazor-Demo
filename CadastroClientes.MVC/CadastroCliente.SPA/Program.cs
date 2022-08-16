f7a07dce83be7354349c2d7d0acf3d530c6b03c8
Jesse Junior <jejunior@support.connectionsacademy.org>
2022-08-15 21:19:37 -0300

    Blazor compilando

--- CadastroClientes.MVC/CadastroCliente.SPA/Program.cs
+++ CadastroClientes.MVC/CadastroCliente.SPA/Program.cs
@@ -1,6 +1,9 @@
 using CadastroCliente.SPA.Data;
+using CadastroClientesCore.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +12,30 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CadastroClientesContext>();
+builder.Services.AddTransient<RepositorioClientes>();
+
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+var supportedCultures = new[] { new CultureInfo("pt-BR") };
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures,
+});
+
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
