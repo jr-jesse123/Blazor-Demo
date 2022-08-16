566e4ba1c0fa83c94362aa80778f271a6996e47a
Jesse Junior <jejunior@support.connectionsacademy.org>
2022-08-16 03:06:46 -0300

    modeloclientecontroller criado

---
+++ CadastroClientes.MVC/CadastroClientes.API/Controllers/ModeloClienteController.cs
@@ -0,0 +1,58 @@
+﻿using CadastroClientes.MVC.Models;
+using CadastroClientesCore.Data;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CadastroClientes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModeloClienteController : ControllerBase
+    {
+        private readonly RepositorioClientes repositorio;
+
+        public ModeloClienteController(RepositorioClientes repositorio)
+        {
+            this.repositorio = repositorio;
+        }
+
+        // GET: api/<ModeloClienteController>
+        [HttpGet]
+        public IEnumerable<ModeloCliente> Get()
+        {
+            return repositorio.ObterTodos();
+        }
+
+        // GET api/<ModeloClienteController>/5
+        [HttpGet("{id}")]
+        public ModeloCliente Get(string cpf)
+        {
+            return repositorio.ObterPorCPF(cpf);
+        }
+
+        // POST api/<ModeloClienteController>
+        [HttpPost]
+        public void Post([FromBody] ModeloCliente cliente)
+        {
+            repositorio.Adicionar(cliente);
+        }
+
+        // PUT api/<ModeloClienteController>/5
+        [HttpPut("{id}")]
+        public void Put(string cpf, [FromBody] ModeloCliente cliente)
+        {
+            var clienteAtual = repositorio.ObterPorCPF(cliente.CPF);
+            
+            repositorio.Atualizar(clienteAtual);
+        }
+
+        // DELETE api/<ModeloClienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(string cpf)
+        {
+            var cliente = repositorio.ObterPorCPF(cpf);
+            repositorio.Remover(cliente);
+        }
+    }
+}
