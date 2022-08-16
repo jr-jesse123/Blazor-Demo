1513d940d12b10a36219247e967f2cb27a99319d
Jesse Junior <jejunior@support.connectionsacademy.org>
2022-08-16 02:49:17 -0300

    pacotes ef adicionados à api

--- CadastroClientes.MVC/CadastroClientes.API/Program.cs
+++ CadastroClientes.MVC/CadastroClientes.API/Program.cs
@@ -1,8 +1,34 @@
+using CadastroClientesCore.Data;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CadastroClientesContext>();
+builder.Services.AddTransient<RepositorioClientes>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+var supportedCultures = new[] { new CultureInfo("pt-BR") };
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(culture: "pt-BR", uiCulture: "pt-BR"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures,
+});
+
 
 var app = builder.Build();
 
